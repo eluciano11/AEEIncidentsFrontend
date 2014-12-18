9011b131e4aeccb1d0974aac5aaf73c54ef8c71a
Daniel Del Rio <danieldr@ens-labs.com>
2014-12-17 21:10:44 -0400

    forgot the parenthesis

--- public/incidents/app/routes/breakdowns/specific.js
+++ public/incidents/app/routes/breakdowns/specific.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 	model: function(params){
-		return Ember.$.getJSON('http://aeeapi.herokuapp.com/api/pueblo_especifico.json?pueblo=' + params.town_name());
+		return Ember.$.getJSON('http://aeeapi.herokuapp.com/api/pueblo_especifico.json?pueblo=' + params.town_name);
 	}
 });
