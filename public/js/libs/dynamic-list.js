5f95f6e9ddbdda0f9cada86ef1e9ba4e7ad529a8
Daniel Del Rio <danny@ens-labs.com>
2015-01-19 13:26:40 -0400

    Wrapped dynamic

--- public/js/libs/dynamic-list.js
+++ public/js/libs/dynamic-list.js
@@ -1,11 +1,13 @@
-function resize_list() {
+(function(){ 
+	function resize_list() {
     var window_size = $(window).height();
     var averias = window_size - 506;
     document.getElementById('dynamic-resize').style.height = window_size + 'px';
     document.getElementById('information').style.height = averias + 'px';
-}
-var resizeTimer;
-$(window).resize(function() {
-    clearTimeout(resizeTimer);
-    resizeTimer = setTimeout(resize_list(), 50);
+	}
+	var resizeTimer;
+	$(window).resize(function() {
+	    clearTimeout(resizeTimer);
+	    resizeTimer = setTimeout(resize_list(), 50);
+	});
 });
